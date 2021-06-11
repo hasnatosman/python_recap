0e858fff9dfbd89c0449b615daf5a895056062e1
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-06-11 10:35:14 +0600

    Index
    Python Practice.

---
+++ main.py
@@ -0,0 +1,22 @@
+
+"""
+In this folder, I will revise python again. It will not be that intensive , but will try to cover
+most of topic.
+The very first code is obviously printing Hello World.
+
+"""
+
+
+print('Hello World!\nI am a pythonista!!')
+
+# It really a easy way to, we can do it different way. Like calling a variable.
+
+greetings = "Hello World!"
+name = 'My name is Hasnat'
+description = 'I am a pythonista !'
+print()
+print('............***.............*****....................')
+
+print(greetings)
+print(name)
+print(description)
