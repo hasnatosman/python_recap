8b7e6426a53231198bc33f63f25120575136d80f
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-06-11 10:37:34 +0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,51 @@
+
+
+"""
+Variable is kind of bucket where you can put anything.
+in variable where you can put different types of value.
+
+"""
+
+a = 10
+b = 20
+
+"""
+here, a and b are two variables. 10 and 20 are values.
+'=' is a assignment operator.
+"""
+x = 10
+print(x)
+x = 20
+print(x)
+"""
+we just assigned 10 in x, that means the value of x is 10.
+but then we reassigned 20 as the value. so that means the value of any variable is
+changeable.
+"""
+
+x0 = 10
+x1 = 20
+_x = 'Hasnat'
+
+
+"""
+We can use any of A to Z and also a to z, firstly using underscore(_), we can even put letter(a-z),
+digit(0-9), but we can't use sign like(@,#,$,%,&) and mostly prohibited are python keywords.
+
+Example: True, False, as, and, class, continue, pass and so on. 
+"""
+
+a = 10
+A = 20
+print(a, A)
+
+x = 10
+y = 20
+print(x,y)
+
+"""
+variable and identifier are very case sensitive. A and a have different meanings and value as well.
+Even using a single extra space will cause an error.
+"""
+
+
