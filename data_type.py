186ddde3bee90e9c99f6a1139a20c0da00f05b5a
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-06-11 11:11:36 +0600

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,36 @@
+"""
+there are lots of data types.
+by using type() function, we will know the data type.
+
+"""
+
+#int
+
+x = 99
+print(x)
+print(type(x))
+
+#float
+
+pi = 3.1416
+print(pi)
+print(type(pi))
+
+
+#string
+
+name = 'Hasnat'
+print(name)
+print(type(name))
+
+
+#bool
+
+x = True
+print(x)
+print(type(x))
+
+"""
+these are the simple data types.
+"""
+
