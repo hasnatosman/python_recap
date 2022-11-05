2d293b5812d9fbc3decabbc5e62610eaf9809669
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2022-11-05 22:51:27 +0600

    Add files via upload

---
+++ func_prac.py
@@ -0,0 +1,11 @@
+# add function................................
+def add(x, y):
+    """ Add two numbers"""
+    z = x + y
+    return z
+
+
+# Driver code....................................
+x, y = 4, 7
+ans = add(x, y)
+print(f'The addition of {x} and {y} is : {ans}')
