9bea3c04be4d2e742b91e69e27fb42614155d50b
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2022-11-05 23:06:38 +0600

    Add files via upload

---
+++ func_prac3.py
@@ -0,0 +1,13 @@
+# a simple python function to check even or odd........
+def even_odd(x):
+    if x % 2 == 0:
+        print(f'{x} is Even number !')
+    else:
+        print(f'{x} is Odd Number !')
+
+
+# Driver code ..................
+even_odd(2)
+even_odd(3)
+even_odd(5)
+even_odd(37700000001222)
