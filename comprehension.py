51a82106d4cf45d1a10f261d7efd29b6796cc593
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2022-11-11 22:17:11 +0600

    Add files via upload

---
+++ comprehension.py
@@ -0,0 +1,8 @@
+# list comprehension................................
+even_number = [i for i in range(11) if i % 2 == 0]
+print(even_number)
+
+sqr = [i ** 2 for i in range(11)]
+print(sqr)
+
+# set comprehension ...............................
